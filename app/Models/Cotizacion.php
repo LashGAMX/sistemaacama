952b197fc097cfc9e7338ec93ae58c4b64c5296f
David Barrita <davidbarrita.db@gmail.com>
2021-03-19 12:39:27 -0600

    model

---
+++ app/Models/Cotizacion.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Cotización extends Model
+{
+    use HasFactory,SoftDeletes;
+    protected $table = '';
+    protected $primaryKey = '';
+    public $timestamps = true;
+
+    protected $fillable = [
+
+    ];
+}
