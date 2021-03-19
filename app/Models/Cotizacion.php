2d560a5a6fe5f4ecf67e222972cf72e0ba9bb070
David Barrita <davidbarrita.db@gmail.com>
2021-03-19 12:39:49 -0600

    nuevo

--- app/Models/Cotizacion.php
+++ app/Models/Cotizacion.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Cotización extends Model
+class Cotizacion extends Model
 {
     use HasFactory,SoftDeletes;
     protected $table = '';
