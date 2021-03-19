263a23b3d4288822be9cfba6d037ab7ebdec781d
David Barrita <davidbarrita.db@gmail.com>
2021-03-19 12:45:13 -0600

    nuevo

--- routes/modules/cotizacion.php
+++ routes/modules/cotizacion.php
@@ -9,4 +9,4 @@ Route::get('cotizacion', [CotizacionController::class,'index']);
 
 Route::get('cotizacion/cotizacion_detalle/{id}', [CotizacionController::class,'show']);
 
-Route::get('cotizacion/cotizacion_detalle/{id}/{idSuc}', [CotizacionController::class,'details']);
+Route::get('cotizacion/cotizacion_detalle/{id}/{idCotizacion}', [CotizacionController::class,'details']);
