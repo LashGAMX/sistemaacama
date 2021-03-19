fe6cb4376c3fc268a6ae0b3a6c2d4a02f719b564
David Barrita <davidbarrita.db@gmail.com>
2021-03-19 12:44:31 -0600

    routas

---
+++ routes/modules/cotizacion.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Clientes\CotizacionController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('cotizacion', [CotizacionController::class,'index']);
+
+Route::get('cotizacion', [CotizacionController::class,'index']);
+
+Route::get('cotizacion/cotizacion_detalle/{id}', [CotizacionController::class,'show']);
+
+Route::get('cotizacion/cotizacion_detalle/{id}/{idSuc}', [CotizacionController::class,'details']);
