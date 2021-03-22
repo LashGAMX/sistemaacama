eca0ab108d0ad60f9b4bd4a4166938489cb5c68c
David Barrita <davidbarrita.db@gmail.com>
2021-03-22 14:51:53 -0600

    cotizacinnn

--- app/Http/Livewire/Cotizacion/Cotizacion.php
+++ app/Http/Livewire/Cotizacion/Cotizacion.php
@@ -3,19 +3,28 @@
 namespace App\Http\Livewire\Cotizacion;
 
 use App\Models\Cotizaciones;
+use App\Models\Norma;
+use App\Models\Clientes;
+use App\Models\Intermediario;
 use Livewire\Component;
 
 class Cotizacion extends Component
 {
 
     public $sw = false;
+    public $controlFormularios = 0;
 
     /**
      * Muestra la Pantalla Inicial
      */
     public function render()
     {
+        $intermediario = Intermediario::withTrashed()->get();
+        $cliente = Clientes::withTrashed()->get();
+        $norma = Norma::withTrashed()->get();
         $model = Cotizaciones::withTrashed()->get();
-        return view('livewire.cotizacion.cotizacion',compact('model'));
+        return view('livewire.cotizacion.cotizacion',compact('model','norma',
+        'cliente','intermediario'));
     }
+
 }
