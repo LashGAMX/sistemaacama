05dc13c199192188f071c6df69e3264a6a3221a1
David Barrita <davidbarrita.db@gmail.com>
2021-03-29 16:39:26 -0600

    cotizacion

--- app/Http/Controllers/Cotizacion/CotizacionController.php
+++ app/Http/Controllers/Cotizacion/CotizacionController.php
@@ -8,6 +8,7 @@ use App\Models\Cotizaciones;
 use App\Models\IntermediariosView;
 use App\Models\Clientes;
 use App\Models\Norma;
+use App\Models\DetallesTipoCuerpo;
 class CotizacionController extends Controller
 {
 
@@ -21,12 +22,59 @@ class CotizacionController extends Controller
         $intermediarios = IntermediariosView::All();
         $cliente = Clientes::All();
         $norma = Norma::All();
+        $clasificacion = DetallesTipoCuerpo::All();
         return view('cotizacion.cotizacion',compact('model','intermediarios','cliente','norma'));
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        //Vista Crear
-        return view('cotizacion.cotizacionCrear');
+        // $clienteManual =   $request->atencionA;
+        // $tipoServicio = $request->tipoServicio;
+        // $atencionA = $request->clienteManual;
+        // $fechaCotizacion = $request->fechaCotizacion;
+        // //Vista Crear
+        // $cotizacion = Cotizaciones::withTrashed()->get();
+        // $num = count($cotizacion);
+        // $num++;
+        //    Cotizaciones::create([
+        //     'Cliente' => $clienteManual,
+        //     'Folio_servicio' => '21-88/'.$num,
+        //     'Cotizacion_folio' => '21-88/'.$num,
+        //     'Empresa' => $this->atencionA,
+        //     'Servicio' => $this->tipoServicio,
+        //     'Fecha_cotizacion' => $this->fechaCotizacion,
+        //     'Supervicion' => 'por Asignar',
+        //     'deleted_at' => NULL,
+        //     'created_by' =>  $this->idUser
+        // ]);
+        // $data = Cotizaciones::latest('Id_cotizacion')->first();
+        // $this->test = $data->Id_cotizacion;
+        // $this->resetValidation();
+
+    }
+
+    public function registrar(Request $request)
+    {
+        $clienteManual =   $request->atencionA;
+        $tipoServicio = $request->tipoServicio;
+        $atencionA = $request->clienteManual;
+        $fechaCotizacion = $request->fechaCotizacion;
+
+        $cotizacion = Cotizaciones::withTrashed()->get();
+        $num = count($cotizacion);
+        $num++;
+           Cotizaciones::create([
+            'Cliente' => $clienteManual,
+            'Folio_servicio' => '21-88/'.$num,
+            'Cotizacion_folio' => '21-88/'.$num,
+            'Empresa' => $atencionA,
+            'Servicio' => $tipoServicio,
+            'Fecha_cotizacion' => $fechaCotizacion,
+            'Supervicion' => 'por Asignar',
+            'deleted_at' => NULL,
+            'created_by' =>  'David Barrita'
+        ]);
+
+        return back();
     }
 }
--- public/js/cotizacion/cotizacion.js
+++ public/js/cotizacion/cotizacion.js
@@ -1,10 +1,40 @@
-// const { some } = require("lodash");
+/**
+ * Programador: Jose David Barrita Lopez
+ * Fecha: 29 de Marzo del 2021
+ * Modulo: Cotización
+ */
+$(function () {
+    console.log("V..02066!");
+    var guardarFormulario = document.getElementById("guardarFormulario");
+    // var btn_primero = document.getElementById('btn_primero');
+    // var bnt_dos_atras = document.getElementById('bnt_dos_atras');
+    var bnt_tres = document.getElementById('bnt_tres');
+    var bnt_dos = document.getElementById('bnt_dos');
 
-console.log('V.03');
+    guardarFormulario.style.display = "none";
+    // btn_primero.style.display = "none";
+    bnt_tres.style.display = "none";
+    // bnt_dos_atras.style.display = "none";
+    bnt_dos.style.display = "none";
+    var nombretabs = document.getElementById("home-tab");
+    nombretabs.innerHTML = "Formulario de Cotización";
+    formulario(1);
+});
+// Funcion para cargar componente
+function obtenerParametros() {
+    var demo2 = $('.demo2').bootstrapDualListbox({
+        nonSelectedListLabel: 'No seleccionado',
+        selectedListLabel: 'Seleccionado',
+        preserveSelectionOnMove: 'Mover',
+        moveOnSelect: true,
+        nonSelectedFilter: 'Filtro'
+    });
+}
+
 // Abrir Modal
 function abrirModal(opt) {
     let option = opt;
-    console.log(typeof(option));
+    console.log(typeof (option));
     console.log('nuevo------------');
     console.log(option)
     $('#modalCotizacionPrincipal').modal('show');
@@ -22,41 +52,89 @@ function abrirModal(opt) {
 
 function formulario(id) {
     console.log(id);
-    console.log(typeof(id));
-    if(id == 1){
+    console.log(typeof (id));
+    if (id == 1) {
         console.log('Formaulario Dos');
-    $("#formularioUno").css("display", "");
-    $("#formularioDos").css("display", "none");
-    $("#formularioTres").css("display", "none");
+        $("#formularioUno").css("display", "");
+        $("#formularioDos").css("display", "none");
+        $("#formularioTres").css("display", "none");
 
-    $("home-tab").css("display", "none");
-    $("nuevo").css("display", "none");
-    $("pro").css("display", "none");
-    $("contact-tab").css("display", "none");
+        $("home-tab").css("display", "none");
+        $("nuevo").css("display", "none");
+        $("pro").css("display", "none");
+        $("contact-tab").css("display", "none");
+
+        // Ver
+        bnt_dos.style.display = "block"
+
+        // No ver
+        guardarFormulario.style.display = "none";
+        bnt_tres.style.display = "none";
     }
 
-    if(id == 2){
-    console.log('Formaulario Dos');
-    $("#formularioUno").css("display", "none");
-    $("#formularioTres").css("display", "none");
-    $("#formularioDos").css("display", "");
+    if (id == 2) {
+        obtenerParametros();
+        console.log('Formaulario Dos');
+        $("#formularioUno").css("display", "none");
+        $("#formularioTres").css("display", "none");
+        $("#formularioDos").css("display", "");
 
-    $("home-tab").css("display", "none");
-    $("nuevo").css("display", "none");
-    $("pro").css("display", "none");
-    $("contact-tab").css("display", "none");
+        $("home-tab").css("display", "none");
+        $("nuevo").css("display", "none");
+        $("pro").css("display", "none");
+        $("contact-tab").css("display", "none");
+
+        //Ver
+        bnt_tres.style.display = "block";
+        // No ver
+        guardarFormulario.style.display = "none";
+        bnt_dos.style.display = "none"
     }
     if (id == 3) {
-        console.log('Formulario Tres');
+
         $("#formularioUno").css("display", "none");
         $("#formularioDos").css("display", "none");
         $("#formularioTres").css("display", "");
 
-    $("home-tab").css("display", "none");
-    $("nuevo").css("display", "none");
-    $("pro").css("display", "none");
-    $("contact-tab").css("display", "none");
+        $("home-tab").css("display", "none");
+        $("nuevo").css("display", "none");
+        $("pro").css("display", "none");
+        $("contact-tab").css("display", "none");
 
+        //Ver
+        guardarFormulario.style.display = "block";
+        bnt_dos.style.display = "none";
+        bnt_tres.style.display = "none";
     }
+}
 
-    }
+$(document).ready(function () {
+    $('#formularioCotizacion').submit(function (e) {
+        console.log('submot');
+        e.preventDefault();
+        var clienteManual = $('#clienteManual').val();
+        var tipoServicio = $('#tipoServicio').val();
+        var atencionA = $('#atencionA').val();
+        var fechaCotizacion = $('#fechaCotizacion').val();
+        var _token = $("#csrf").val();
+
+        $.ajax({
+            url: "cotizacion/save",
+            type: "POST",
+            data: {
+                clienteManual: clienteManual,
+                tipoServicio: tipoServicio,
+                atencionA: atencionA,
+                fechaCotizacion: fechaCotizacion,
+                _token: _token
+
+            },
+            success: function (response) {
+                if (response) {
+                    console.log('nuevo-pro-2020');
+                }
+            }
+
+        });
+    });
+});
