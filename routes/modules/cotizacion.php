f237339df9853c874f8a4aa721320d47e03b44c5
LashGAMX <alber_199648@hotmail.com>
2021-04-05 13:10:57 -0500

    commit

--- routes/modules/cotizacion.php
+++ routes/modules/cotizacion.php
@@ -1,5 +1,5 @@
 <?php
-
+ 
 use App\Http\Controllers\Cotizacion\Cotizacion2Controller;
 use App\Http\Controllers\Cotizacion\CotizacionController;
 use App\Http\Controllers\Cotizacion\CotizacionConfigController;
@@ -12,6 +12,10 @@ Route::post('cotizacion/obtenerClasificacion', [CotizacionController::class,'obt
 
 Route::get('cotizacion/create', [Cotizacion2Controller::class,'create']);
 Route::post('cotizacion/getSubNorma', [Cotizacion2Controller::class,'getSubNorma']);
+Route::post('cotizacion/getDatos2', [Cotizacion2Controller::class,'getDatos2']);
+Route::post('cotizacion/setCotizacion', [Cotizacion2Controller::class,'setCotizacion']); 
+Route::get('cotizacion/fecha', [Cotizacion2Controller::class,'fecha']);
  
 Route::post('cotizacion/obtenerHistorico', [CotizacionController::class,'obtenerHistorico'])->name('cotizacion.obtenerHistorico');
 Route::post('cotizacion/duplicarCotizacion', [CotizacionController::class,'duplicarCotizacion'])->name('cotizacion.duplicarCotizacion');
+    
