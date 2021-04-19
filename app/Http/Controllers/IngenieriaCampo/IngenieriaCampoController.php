c4c3a658fa1b0f3891aadd8eea9bd02b951d27e6
David Barrita <davidbarrita.db@gmail.com>
2021-04-19 14:17:44 -0500

    Ingenieria Campo

---
+++ app/Http/Controllers/IngenieriaCampo/IngenieriaCampoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\IngenieriaCampo;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IngenieriaCampoController extends Controller
+{
+    //----------------------------------------------------/
+    /****************************************************/
+    //MODULO DE CONTROL DE INSTRUMENTOS//
+
+
+}
