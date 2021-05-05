beb4ed7b5f17ca2c2bc9459858823bf4aa9a1e37
LashGAMX <alber_199648@hotmail.com>
2021-05-05 08:06:18 -0500

    commit ruta beto

---
+++ app/Http/Controllers/Beto/BetoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Beto;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BetoController extends Controller
+{
+    //
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -92,6 +92,10 @@ class RouteServiceProvider extends ServiceProvider
                     ->namespace($this->namespace)
                     ->prefix('admin') 
                     ->group(base_path('routes/modules/librerias.php'));
+                    Route::middleware('web', 'auth')
+                    ->namespace($this->namespace)
+                    ->prefix('admin') 
+                    ->group(base_path('routes/modules/beto.php'));
         });
     }
 
