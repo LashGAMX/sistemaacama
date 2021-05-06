63563302587b40a422efaa51e6034a28e39898ef
IsaacVillanuevaAlonso <isaacyannis@gmail.com>
2021-05-06 13:33:35 -0500

    commit interfaz de formulas

--- resources/views/analisisQ/crear_formula.blade.php
+++ resources/views/analisisQ/crear_formula.blade.php
@@ -9,46 +9,39 @@
   </h6>
 
   @stop
-    <div class="container">
-        <div class="row">
-            <div class="col-md-2">
-                <button type="submit" class="btn btn-success">Guardar</button>
-                <button type="button" class="btn btn-secondary" >Probar</button>
-            </div>
-        </div>
-    
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2">
+        <button type="submit" class="btn btn-success">Guardar</button>
+      </div>
+      <div class="col-md-2">
+        <button type="button" class="btn btn-secondary" >Probar</button>
+      </div>
     </div>
-    
-    <div class="container"> 
-    <div class="row">  
-         
-        <div class="col-md-3">
-           
-           
-            <input type="text" wire:model='formula' class="form-control" placeholder="Formula">
-        </div>
-        <div class="col-md-3">
-            
-            <input type="text" wire:model='Prom_Dmin' class="form-control" placeholder="Formula sistema">    
-        </div>
-        <div class="col-md-3">
-           
-            <button type="submit" class="btn btn-danger">Asignar</button>
-        </div>
-        
-        </div>
+    <div class="row">
+      <div class="col-md-3">
+        <input type="text" wire:model='formula' class="form-control" placeholder="Formula">
+      </div>
+      <div class="col-md-3">
+        <input type="text" wire:model='Prom_Dmin' class="form-control" placeholder="Formula sistema">  
+      </div>
+      <div class="col-md-3">
+        <button type="submit" class="btn btn-danger">Asignar</button>
+      </div>
+      <div class="col-md-3">
+        <textarea>Reglas</textarea>
+      </div>
     </div>
-    
-
+    <div class="row">
         <div class="col-md-2">
-        <label for="">Área      </label>
-        <select>
-            <option value="0">Selecciona...</option>
-            <option value="1">ABS</option>
-            <option value="2">POTABLE</option>
-            <option value="2">FISICOQUIMICO</option>
-          </select>
-        </div> 
+            <label for="">Área</label>
+            <select>
+                <option value="0">Selecciona...</option>
+                <option value="1">ABS</option>
+                <option value="2">POTABLE</option>
+                <option value="2">FISICOQUIMICO</option>
+              </select>
+        </div>
         <div class="col-md-2">
             <label for="">Técnica</label>
             <select>
@@ -58,6 +51,11 @@
                 <option value="2">FISICOQUIMICO</option>
               </select>
         </div>
+      </div>
+  </div>
+
+
+  
     
         
 
