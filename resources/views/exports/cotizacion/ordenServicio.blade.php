ac318f1cc9df2fbbc7714ab477cefa6eb35590b5
LashGAMX <alber_199648@hotmail.com>
2021-05-24 17:51:16 -0500

    commit

--- resources/views/exports/cotizacion/ordenServicio.blade.php
+++ resources/views/exports/cotizacion/ordenServicio.blade.php
@@ -4,130 +4,102 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> --}}
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/pdf/style.css')}}">
  
     <title>Solicitud {{$model->Folio_servicio}}</title>
 </head>
-<body>
+<body> 
     <div class="container" id="pag">
-        {{-- <div class="row">
-            <div class="col-12" style="display: flex">
-                <img src="https://dev.sistemaacama.com.mx//storage/Logo_sin_fondo.png" style="width: 100;">
-                <center><div style="font-size: 11px" class="verdeClaro">LABORATORIO DE ANÁLISIS DE CALIDAD <br> DEL AGUA Y MEDIOS AMBIENTALES S.A DE C.V</div></center>
+        <div class="row">
+            <div class="col align-self-end">
+                <P align="right">FOLIO COTIZACIÓN: {{$model->Folio_servicio}}<p>
             </div>
-        </div> --}}
-        <table>
-            <tr>
-                <td style="width: 30%">Nombre de la empresa</td>
-                <td>
-                    <div class="bordeTabla" style="height: 50px;">
-                        {{$model->Empresa}}    
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="width: 30%">Dirección</td>
-                <td>
-                    <div class="bordeTabla" style="height: 50px;">
-                        {{$model->Direccion}}
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="width: 30%">Contacto</td>
-                <td>
-                    <div class="bordeTabla" style="height: 50px;">
-                        {{$model->Nom_con}} {{$model->Nom_pat}}
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="width: 30%">Teléfono</td>
-                <td>
-                    <div class="bordeTabla" style="height: 40px;">
-                        
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="width: 30%" style="height: 50px;">Servicio</td>
-                <td>
-                    <div class="bordeTabla" style="height: 50px;">
-                        {{$model->Servicio}}
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="width: 30%">Norma</td>
-                <td>
-                    <div class="bordeTabla" style="height: 50px;">
-                        {{$model->Clave_norma}}
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="width: 30%">Parametros</td>
-                <td>
-                    <div class="bordeTabla" style="height: 150px;"> 
-                        @foreach ($parametros as $item)
-                            {{$item->Parametro}}
-                        @endforeach 
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="width: 30%">Parametros especiales</td>
-                <td>
-                    <div class="bordeTabla" style="height: 100px;color:red">
-                        @php
-                            $cont = 0;
-                        @endphp
-                        @foreach ($parametros as $item)
-                            @if ($item->Extra != 0)
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table">
+                    <tr>
+                        <td style="width: 30%" class="">Nombre de la empresa</td>
+                        <td class="border">
+                                {{$model->Empresa}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Dirección</td>
+                        <td style="width: 70%" class="border">
+                            {{$model->Direccion}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Contacto</td>
+                        <td class="border">
+                            {{$model->Nom_con}} {{$model->Nom_pat}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Teléfono</td>
+                        <td class="border">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%" style="height: 50px;">Servicio</td>
+                        <td class="border">
+                            {{$model->Servicio}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Norma</td>
+                        <td class="border">
+                            {{$model->Clave_norma}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Parametros</td>
+                        <td class="border">
+                            @foreach ($parametros as $item)
+                                    {{$item->Parametro}}
+                                @endforeach 
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Parametros especiales</td>
+                        <td class="border" style="color:red">
                                 @php
-                                $cont++;    
+                                    $cont = 0;
                                 @endphp
-                                {{$item->Parametro}}
-                            @endif    
-                        @endforeach
-                        @if ($cont == 0)
-                            @php
-                                echo "Sin parametros especiales";
-                            @endphp
-                        @endif
-                    </div>
-                </td>
-            </tr>
+                                @foreach ($parametros as $item)
+                                    @if ($item->Extra != 0)
+                                        @php
+                                        $cont++;    
+                                        @endphp
+                                        {{$item->Parametro}}
+                                    @endif    
+                                @endforeach
+                                @if ($cont == 0)
+                                    @php
+                                        echo "Sin parametros especiales";
+                                    @endphp
+                                @endif
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Fecha de muestreo</td>
+                        <td class="border">
+                            {{$model->Fecha_muestreo}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Observaciones</td>
+                        <td class="border">
+                                {{$model->Observacion}}
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
             <tr>
-                <td style="width: 30%">Fecha de muestreo</td>
-                <td>
-                    <div class="bordeTabla" style="height: 50px;">
-                        
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="width: 30%">Observaciones</td>
-                <td>
-                    <div class="bordeTabla" style="height: 50px;">
-                        {{$model->Observacion}}
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="width: 30%">Observaciones</td>
-                <td>
-                    <div class="bordeTabla" style="height: 50px;">
-                        
-                    </div>
-                </td>
-            </tr>
-        </table>
-        <table>
-            <tr>
-                <td>{!! DNS2D::getBarcodeHTML($model->Folio_servicio, 'QRCODE') !!}</td>
+                <td>{!! DNS2D::getBarcodeHTML(url('clientes/orden_servicio/'.$model->Id_cotizacion), 'QRCODE') !!}</td>
             </tr>
         </table>
         {{-- <div class="col-md-12">
