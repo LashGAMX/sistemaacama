7d974d9d7a6987bbd1cdb07d8c9407b0315f837a
LashGAMX <alber_199648@hotmail.com>
2021-05-31 17:04:54 -0500

    commit

---
+++ app/Models/Localidad.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Localidad extends Model
+{
+    use HasFactory,SoftDeletes;
+    protected $table = 'localidades';
+    protected $primaryKey = 'Id_localidad';
+    public $timestamps = true;
+    protected $fillable = [
+        'Id_municipio',
+        'Clave',
+        'Nombre',
+
+    ];
+}
+ 
