5db5efd37cea66fdae61238a410339efacfa7aff
IsaacVillanuevaAlonso <isaacyannis@gmail.com>
2021-06-01 14:47:53 -0500

    commit

---
+++ app/Http/Livewire/Config/Localidad.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Config;
+
+use Livewire\Component;
+
+class Localidad extends Component
+{
+    public function render()
+    {
+        return view('livewire.config.localidad');
+    }
+}
