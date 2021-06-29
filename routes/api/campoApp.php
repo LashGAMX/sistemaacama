0c1a85875648ca7a87c71642c7203ba4fcd3c576
LashGAMX <alber_199648@hotmail.com>
2021-06-29 17:59:32 -0500

    commit

--- routes/api/campoApp.php
+++ routes/api/campoApp.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'app'], function () {
     Route::get('login/{user}/{pass}', [CampoAppController::class,'login']);
     Route::get('getUser', [CampoAppController::class,'getUser']);
+    Route::post('user', [CampoAppController::class,'user']);
 });
     
