d1c2cfcc878037f79f6de9579a7ec6659425e0a1
LashGAMX <alber_199648@hotmail.com>
2021-07-13 08:55:15 -0500

    commit

--- app/Http/Controllers/Api/CampoAppController.php
+++ app/Http/Controllers/Api/CampoAppController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Usuario;
 use App\Models\UsuarioApp;
 use Illuminate\Http\Request;
 
@@ -12,7 +11,7 @@ class CampoAppController extends Controller
     public function login(Request $request)
     {
         $model = UsuarioApp::where('User',$request->user)
-            ->where('UserPass',$request->pass)
+            ->where('UserPass',$request->pass) 
             ->get();
         if($model->count()){
             $success = true;    
@@ -20,7 +19,7 @@ class CampoAppController extends Controller
             $success = false;
         }
         $data = array(
-            'usuarios' => UsuarioApp::all(),
+            'usuarios' => UsuarioApp::all(), 
             'response' => $success,
             'data' => $model,  
         );
--- routes/api/campoApp.php
+++ routes/api/campoApp.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'app'], function () {
     Route::post('login', [CampoAppController::class,'login']);
 });
-    
+     
