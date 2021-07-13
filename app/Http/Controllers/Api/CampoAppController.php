0185467c0ea7cb4a93866d98858a7b9bba07795d
LashGAMX <alber_199648@hotmail.com>
2021-07-13 07:25:26 -0500

    commuit

--- app/Http/Controllers/Api/CampoAppController.php
+++ app/Http/Controllers/Api/CampoAppController.php
@@ -6,24 +6,44 @@ use App\Http\Controllers\Controller;
 use App\Models\Usuario;
 use App\Models\UsuarioApp;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CampoAppController extends Controller
 {
     public function login(Request $request)
     {
+        $idMuestreador = 0;
         $model = UsuarioApp::where('User',$request->user)
             ->where('UserPass',$request->pass)
             ->get();
         if($model->count()){
+            foreach($model as $item){
+                $idMuestreador = $item->Id_muestreador;
+            }
             $success = true;    
         }else{
             $success = false;
         }
         $data = array(
             'usuarios' => UsuarioApp::all(),
+            'solicitudes' => DB::table('ViewSolicitudGenerada')->where('Id_muestreador', $idMuestreador)->get(),
             'response' => $success,
             'data' => $model,  
         );
         return response()->json($data); 
     } 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
