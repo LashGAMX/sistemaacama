93ef0a88766027bcafae60de32766b1f50e9dce1
Christian Gonzalez <christian.gc099@gmail.com>
2021-08-12 17:55:25 -0500

    commit > Módulo Ingresar

--- public/js/ingresar/ingresar.js
+++ public/js/ingresar/ingresar.js
@@ -74,48 +74,106 @@ function mostrarDatos()
         } );  
 }
 
-function generarDatos(idSolicitud, folio) 
-{
-    //console.log("Dentro de Generar");
-    let tabla = document.getElementById('divTablaDatos');
-    let tab = '';
-    
-    $.ajax({
-        url: base_url + '/admin/ingresar/listar', //archivo que recibe la peticion
-        type: 'POST', //método de envio
-        data: {
-            idSolicitud:idSolicitud,
-            folio:folio,
-            _token: $('input[name="_token"]').val(),
-        },
-        dataType: 'json', 
-        async: false, 
-        success: function (response) {
-          //console.log("Dentro de función Ajax");
-          console.log(response);
-          tab += '<table id="tablaDatos" class="table">';
-          tab += '    <thead class="thead-dark">';
-          tab += '        <tr>';
-          tab += '            <th style="font-size: 200%">Folio</th>';
-          tab += '            <th style="font-size: 200%">Descarga</th>';
-          tab += '            <th style="font-size: 200%">Cliente o Intermediario</th>';
-          tab += '            <th style="font-size: 200%">Empresa</th>';
-          tab += '            <th style="font-size: 200%">Hora Recepción</th>';
-          tab += '            <th style="font-size: 200%">Hora Entrada</th>';
-          tab += '        </tr>';
-          tab += '    </thead>';
-          tab += '    <tbody>';
-          $.each(response.model, function (key, item) {
-            tab += '<tr>';
-            tab += '    <td style="font-size: 150%">'+item.Folio+'</td>';
-            tab += '    <td style="font-size: 150%">'+item.Descarga+'</td>';
-            tab += '    <td style="font-size: 150%">'+item.NomInter+'</td>';
-            tab += '    <td style="font-size: 150%">'+item.Nombre+'</td>';            
-            tab += '</tr>';
-          });
-          tab += '    </tbody>';
-          tab += '</table>';
-          tabla.innerHTML = tab;
-        }
-    });  
+function validacionFecha(horaE, horaR, btnI){
+    let horaEntrada = moment(document.getElementById(horaE).value, "YYYY-MM-DD HH:mm:ss");
+    let horaRecepcion = moment(document.getElementById(horaR).value, "DD/MM/YYYY HH:mm:ss A");
+    let btnIngresar = document.getElementById(btnI);
+
+    if((moment(horaEntrada)).isBefore(horaRecepcion)){
+        btnIngresar.setAttribute("disabled", true);
+        console.log("dentro de if en validacionFecha");
+        alert("La hora de entrada no puede ser inferior a la hora de recepción");
+    }else{
+        btnIngresar.setAttribute("disabled", false);
+        console.log("dentro de else en validacionFecha");
+    }
 }
+
+function setIngresar(){
+    $.ajax({
+        type: "POST",
+        url: base_url + '/admin/ingresar/ingresar',
+        data: {
+            folio: $("#folio").val(),
+            descarga: $("#descarga").val(),
+            cliente: $("#cliente").val(),
+            empresa: $("#empresa").val(),
+            horaEntrada: moment($("#hora_entrada").val()).format("YYYY-MM-DD HH:mm:ss")
+        },
+        dataType: "json",
+        async: false,
+        success: function (response) {
+            console.log(response);
+            swal("Ingreso!", "Ingreso guardado correctamente!", "success");
+        }
+    });
+}
+
+window.addEventListener("load", function(){
+    let texto;
+    let folio = document.getElementById("folio");
+    let descarga = document.getElementById("descarga");
+    let cliente = document.getElementById("cliente");
+    let empresa = document.getElementById("empresa");
+    let horaRecepcion = document.getElementById("hora_recepcion");
+    let horaEntrada = document.getElementById("hora_entrada");
+    let mensaje = document.getElementById("mensajeBusqueda");
+    let btnIngresar = document.getElementById("btnIngresar");
+          
+    //btnIngresar.setAttribute("disabled", true);
+
+    document.getElementById("texto").addEventListener("keyup", function(){
+        texto = document.getElementById("texto").value;
+                
+        $.ajax({
+            url: base_url + '/admin/ingresar/buscador',
+            type: 'GET',
+            data: {
+                texto:texto,
+                _token: $('input[name="_token"]').val()
+            },
+            dataType: 'json',
+            async: false,
+            success: function (response) {
+                //console.log("Dentro de función Ajax");                
+                console.log(response);
+
+                if(texto.length != 0){
+                    if(response.solicitud !== null){
+                        folio.value = response.solicitud.Folio_servicio;
+                        descarga.value = response.solicitud.Descarga;
+                        cliente.value = response.solicitud.Empresa;
+                        empresa.value = response.solicitud.Nombres;
+                        horaRecepcion.value = moment(response.model.created_at).format("DD/MM/YYYY HH:mm:ss A");
+                        horaEntrada.value = moment(response.model.Hora_entrada, "YYYY-MM-DD HH:mm:ss").format("YYYY-MM-DDTHH:mm:ss");
+                        mensaje.innerHTML = "";
+
+                        //btnIngresar.setAttribute("disabled", true);
+                        console.log("dentro de if en response.solicitud");
+                    }else{                    
+                        folio.value = "";
+                        descarga.value = "";
+                        cliente.value = "";
+                        empresa.value = "";
+                        horaRecepcion.value = "";
+                        horaEntrada.value = "";
+                        mensaje.innerHTML = "No se encontraron registros";
+                        
+                        btnIngresar.setAttribute("disabled", true);
+                        console.log("dentro de else en response.solicitud");
+                    }
+                }else{
+                        folio.value = "";
+                        descarga.value = "";
+                        cliente.value = "";
+                        empresa.value = "";
+                        horaRecepcion.value = "";
+                        horaEntrada.value = "";
+                        mensaje.innerHTML = "";
+                        btnIngresar.setAttribute("disabled", true);
+                        console.log("dentro de else en texto.length");
+                }                
+            },
+        });        
+    });
+});
