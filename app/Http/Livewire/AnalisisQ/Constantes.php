51838cd30c584972fd2fe7f5e3cd230c96ad9196
IsaacVillanuevaAlonso <isaacyannis@gmail.com>
2021-09-27 17:53:32 -0500

    commit

---
+++ app/Http/Livewire/AnalisisQ/Constantes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\AnalisisQ;
+
+use Livewire\Component;
+
+class Constantes extends Component
+{
+    public function render()
+    {
+        return view('livewire.analisis-q.constantes');
+    }
+}
