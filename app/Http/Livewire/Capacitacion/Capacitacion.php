081cbbc174f026e4aee0f084cf0778ff73ffb68c
IsaacVillanuevaAlonso <isaacyannis@gmail.com>
2021-10-07 09:19:10 -0500

    commit

--- app/Http/Livewire/Capacitacion/Capacitacion.php
+++ app/Http/Livewire/Capacitacion/Capacitacion.php
@@ -1,26 +1,17 @@
 <?php
 
 namespace App\Http\Livewire\Capacitacion;
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> 44462f9efd4d623bdebdd1e97a10d6d1c4737431
 use App\Models\Grupo;
 use Livewire\Component;
 
 class Capacitacion extends Component
 {
-<<<<<<< HEAD
-=======
-    //Cuando es publica no es necesario pasarla por medio de compact
->>>>>>> 44462f9efd4d623bdebdd1e97a10d6d1c4737431
     public $buscar;
     public $grupo;
     public $comentario;
     public $sw = false;
 
-<<<<<<< HEAD
     public function render()
     {
         $model = Grupo::all();
@@ -28,32 +19,16 @@ class Capacitacion extends Component
     }
     public function create()
     {
-=======
-    //Método que escucha constantemente
-    public function render()
-    {
-        $model = Grupo::all();
-        return view('livewire.capacitacion.capacitacion', compact('model'));
-    }
-
-    public function create(){
->>>>>>> 44462f9efd4d623bdebdd1e97a10d6d1c4737431
         Grupo::create([
             'Grupo' => $this->grupo,
             'Comentarios' => $this->comentario
         ]);
     }
 
-<<<<<<< HEAD
     public function btnCrear()
     {
         if($this->sw == false)
         {
-=======
-    //Si es una variable publica para usarse debe referenciarse por medio de this
-    public function btnCrear(){
-        if($this->sw == false ){
->>>>>>> 44462f9efd4d623bdebdd1e97a10d6d1c4737431
             $this->sw = true;
         }else{
             $this->sw = false;
