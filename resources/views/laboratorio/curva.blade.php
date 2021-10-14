0ab8154f88ead43cedc685e3805f3ee686def468
IsaacVillanuevaAlonso <isaacyannis@gmail.com>
2021-10-14 00:51:46 -0500

    commit

--- resources/views/laboratorio/curva.blade.php
+++ resources/views/laboratorio/curva.blade.php
@@ -12,11 +12,42 @@
   <div class="container-fluid"> 
     <div class="row">
       <div class="col-md-4">
-        <a href="{{url('/admin/analisisQ/formulas/crear')}}" class="btn btn-success"><i class="voyager-plus"></i> Crear</a>
-        <a href="{{url('/admin/analisisQ/formulas/nivel')}}" class="btn btn-primary"><i class="fa fa-level-up-alt"></i> Nivel formula</a>
-        <a href="{{url('/admin/analisisQ/formulas/constantes')}}" class="btn btn-warning"><i class="fab fa-cuttlefish"></i> Constante</a>
+        <button class="btn btn-success" data-toggle="modal" data-target="#modalCrear"><i class="voyager-plus"></i> Crear</button>
+        
       </div>
       <div class="col-md-4">
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="exampleFormControSelect1">Lote</label>
+              <select class="form-control">
+                  <option value="0">Sin seleccionar</option>
+                  <option value="1">Acreditados</option>
+                  <option value="2">Metales alimentos</option>
+                  <option value="3">Metales pesados en biosolidos</option>
+                  <option value="4">Metales potable</option>
+                  <option value="5">Metales purificadora</option>
+                  <option value="6">Metales residual</option>
+                  <option value="7">Miliequivalentes</option>
+                  <option value="8">No acreditados</option>
+                </select>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="exampleFormControSelect1">Fórmula</label>
+              <select class="form-control">
+                  <option value="0">Sin seleccionar</option>
+                  <option value="1">Acreditados</option>
+                  <option value="2">Metales alimentos</option>
+                  <option value="3">Metales pesados en biosolidos</option>
+                  <option value="4">Metales potable</option>
+                  <option value="5">Metales purificadora</option>
+                  <option value="6">Metales residual</option>
+                  <option value="7">Miliequivalentes</option>
+                  <option value="8">No acreditados</option>
+                </select>
+          </div>
+        </div>
   
       </div>
       <div class="col-md-4">
@@ -28,22 +59,28 @@
           <thead class="thead-dark">
                   <tr>
                    
-                      <th>Parametro</th>
-                      <th>Técnica</th>
-                      <th>Area de análisis</th>
+                      <th>Lote</th>
                       <th>Formula</th>
-                      <th>Formula Sistema</th>
-                      <th>Resultado</th>
+                      <th>Técnica</th>
+                      <th>STD</th>
+                      <th>Concentración</th>
+                      <th>ABS1</th>
+                      <th>ABS2</th>
+                      <th>ABS3</th>
+                      <th>Promedio</th>
                     </tr>
               </thead>
               <tbody>
-                @foreach ($formulas as $item) 
-                <td>{{$item->Id_parametro}}</td>
+                @foreach ($model as $item) 
+                <td>{{$item->Id_lote}}</td>
+                <td>{{$item->Id_formula}}</td>
                 <td>{{$item->Id_tecnica}}</td>
-                <td>{{$item->Id_area}}</td>
-                <td>{{$item->Formula}}</td>
-                <td>{{$item->Formula_sistema}}</td>
-                <td>{{$item->Resultado}}</td>
+                <td>{{$item->STD}}</td>
+                <td>{{$item->Concentracion}}</td>
+                <td>{{$item->ABS1}}</td>
+                <td>{{$item->ABS2}}</td>
+                <td>{{$item->ABS3}}</td>
+                <td>{{$item->Promedio}}</td>
                 <td>
                   <button type="button" class="btn btn-warning">
                   <i class="voyager-edit"></i> <span hidden-sm hidden-xs>editar</span> </button>
@@ -55,6 +92,54 @@
           </table>
       </div>
     </div>
+        <!-- Modal -->
+        <div wire:ignore.self class="modal fade" id="modalCrear" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+              <div class="modal-content">
+               <form wire:submit.prevent="create">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Nueva constante</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body"> 
+                    <div class="row">
+                        <div class="col-md-6">
+                            
+                            <label for="">Concentración</label>
+                            <input type="text" id='concentracion' class="form-control" placeholder="Concentración">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="">ABS1</label>
+                            <input type="text" id='ABS1' class="form-control" placeholder="ABS1">
+                        </div>
+                        <div class="col-md-6">
+                          <label for="">ABS2</label>
+                          <input type="text" id='ABS1' class="form-control" placeholder="ABS1">
+                      </div>
+                      <div class="col-md-6">
+                        <label for="">ABS3</label>
+                        <input type="text" id='ABS1' class="form-control" placeholder="ABS1">
+                    </div>
+                    <div class="col-md-6">
+                      <label for="">Promedio</label>
+                      <input type="text" id='promedio' class="form-control" placeholder="Promedio">
+                  </div>
+                        
+                    </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                  <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
+              </form>
+              </div>
+            </div>
+            
+           </div>
+
+
     </div> 
   @stop
 
