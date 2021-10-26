9f5ae22e4bba60b18cd1ff272b7be97ab9991980
LashGAMX <alber_199648@hotmail.com>
2021-10-26 16:42:17 -0500

    commit

--- routes/modules/ingresar.php
+++ routes/modules/ingresar.php
@@ -12,20 +12,7 @@ Route::group(['prefix' => 'ingresar'], function () {
     Route::post('buscarFolio', [IngresarController::class,'buscarFolio']);
     Route::post('setIngresar', [IngresarController::class,'setIngresar']);
 
-    // //Ruta para consultar fecha fin de muestreo siralab
-    // Route::get('siralabFecha', [IngresarController::class, 'fechaFinSiralab']);
+  
 
-    // //Route::post('ingresar', [IngresarController::class, 'setIngresar']);
-    // //Route::post('listar', [IngresarController::class, 'generar']);
-
-    // //RUTA PARA OBTENER LA FECHA DE CONFORMACIÓN
-    // Route::get('fechaConformacion', [IngresarController::class, 'fechaConformacion']);
-
-    // //RUTA PARA OBTENER LA PROCEDENCIA CON PREVIA COTIZACIÓN
-    // Route::get('procedencia', [IngresarController::class, 'procedencia']);
-
-
-    // //-------------------Módulo solicitudes---------------------
-    // Route::get('generar', [IngresarController::class, 'genera2']);
-    // Route::get('/generar/buscarSol', [IngresarController::class, 'buscadorGen']);
+   
 }); 
