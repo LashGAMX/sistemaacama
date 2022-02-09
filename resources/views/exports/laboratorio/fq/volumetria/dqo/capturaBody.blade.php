b790324663afe7e94e1b681d99e82cb8d6830a69
LashGAMX <alber_199648@hotmail.com>
2022-02-09 17:59:41 -0600

    Co-authored-by: ChristianGonzalez099 <ChristianGonzalez099@users.noreply.github.com> Co-authored-by: IsaacVillanuevaAlonso <IsaacVillanuevaAlonso@users.noreply.github.com>

---
+++ resources/views/exports/laboratorio/fq/volumetria/dqo/capturaBody.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('/public/css/laboratorio/fq/volumetria/dqo/dqoPDF.css')}}">
+    <title>Captura PDFs</title>
+</head>
+<body>
+    <p id='curvaProcedimiento'>Procedimiento</p>
+
+    <div id="contenidoCurva">
+        <?php echo html_entity_decode($textoProcedimiento[0]);?>
+    </div>
+
+    <br>
+
+    <div class="contenedorTabla">
+        <table autosize="1" class="table table-borderless" id="tablaDatos">
+            <thead>
+
+                <tr>
+                    <th class="nombreHeader" colspan="7">
+                        Resultado de las muestras
+                    </th>                    
+                </tr>                
+
+                <tr>
+                    <th class="tableCabecera anchoColumna">No. de muestras</th>
+                    <th class="tableCabecera anchoColumna">Volumen de muestra (mL)</th>
+                    <th class="tableCabecera anchoColumna">Volumen del titulante</th>
+                    <th class="tableCabecera anchoColumna">DQO mg/L</th>
+                    <th class="tableCabecera anchoColumna">Observaciones</th>                    
+                    <th class="anchoColumna"></th>
+                    <th class="anchoColumna"></th>
+                </tr>
+            </thead>
+    
+            <tbody>
+                @for ($i = 0; $i < @$dataLength ; $i++)
+                    <tr>
+                        <td class="tableContent">{{@$data[$i]->Folio_servicio}}</td>
+                        <td class="tableContent">{{@$data[$i]->Vol_muestra}}</td>
+                        <td class="tableContent">{{@$data[$i]->Titulo_blanco}}</td>
+                        <td class="tableContent">{{@$data[$i]->Resultado}}</td>
+                        <td class="tableContent">{{@$data[$i]->Observacion}}</td>
+                        <td class="tableContent">{{@$data[$i]->Descripcion}}</td>
+                        <td class="tableContent">{{@$data[$i]->Control}}</td>                                              
+                    </tr>                
+                @endfor
+            </tbody>        
+        </table>  
+    </div>    
+</body>
+</html>
---
+++ resources/views/exports/laboratorio/fq/volumetria/nitrogenoTotal/capturaBody.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('/public/css/laboratorio/fq/volumetria/nitrogenoTotal/nitrogenoTotalPDF.css')}}">
+    <title>Captura PDF</title>
+</head>
+<body>
+    <p id='curvaProcedimiento'>Procedimiento</p>
+
+    <div id="contenidoCurva">
+        <?php echo html_entity_decode($textoProcedimiento->Texto);?>
+    </div>
+
+    <br>
+
+    <div class="contenedorTabla">
+        <table autosize="1" class="table table-borderless" id="tablaDatos">
+            <thead>
+
+                <tr>
+                    <th class="nombreHeader" colspan="8">
+                        Resultado de las muestras
+                    </th>                    
+                </tr>                
+
+                <tr>
+                    <th class="tableCabecera anchoColumna">No. de muestra</th>
+                    <th class="tableCabecera anchoColumna">VOL. TITULANTE BLANCO</th>
+                    <th class="tableCabecera anchoColumna">VOL. MUESTRA</th>
+                    <th class="tableCabecera anchoColumna">VOL. TITULANTE MUESTRA</th>
+                    <th class="tableCabecera anchoColumna">NITRÓGENO AMONIACAL (N-NH3) mg/L</th>
+                    <th class="tableCabecera anchoColumna">Observaciones</th>
+                    <th class="anchoColumna"></th>
+                    <th class="anchoColumna"></th>
+                </tr>
+            </thead>
+    
+            <tbody>
+                {{-- @for ($i = 0; $i < 100 ; $i++) --}}
+                    <tr>
+                        <td class="tableContent">PRUEBA</td>
+                        <td class="tableContent">PRUEBA</td>
+                        <td class="tableContent">PRUEBA</td>
+                        <td class="tableContent">PRUEBA</td>
+                        <td class="tableContent">PRUEBA</td>
+                        <td class="tableContent">PRUEBA</td>
+                        <td class="tableContent">PRUEBA</td>
+                        <td class="tableContent">PRUEBA</td>
+                    </tr>                
+                {{-- @endfor --}}
+            </tbody>        
+        </table>  
+    </div>    
+</body>
+</html>
