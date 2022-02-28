4cb064bb43665691e14b7a08ee38734555367fed
LashGAMX <alber_199648@hotmail.com>
2022-02-28 16:57:49 -0600

    commit

--- app/Http/Controllers/Informes/InformesController.php
+++ app/Http/Controllers/Informes/InformesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Informes;
 use App\Http\Controllers\Controller;
 use App\Models\TipoReporte;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InformesController extends Controller
 {
@@ -13,5 +14,57 @@ class InformesController extends Controller
     {
         $tipoReporte = TipoReporte::all();
         return view('informes.informes',compact('tipoReporte'));
+    }
+
+    public function pdfSinComparacion(){
+        //Opciones del documento PDF
+        $mpdf = new \Mpdf\Mpdf([
+            'orientation' => 'P',
+            'format' => 'letter',
+            'margin_left' => 10,
+            'margin_right' => 10,
+            'margin_top' => 90,
+            'margin_bottom' => 45,
+            'defaultheaderfontstyle' => ['normal'],
+            'defaultheaderline' => '0'
+        ]);
+
+        //Establece la marca de agua del documento PDF
+        $mpdf->SetWatermarkImage(
+            asset('/public/storage/MembreteVertical.png'),
+            1,
+            array(215, 280),
+            array(0, 0),
+        );
+
+        $mpdf->showWatermarkImage = true;
+
+        //Recupera el nombre de usuario y firma
+        $usuario = DB::table('users')->where('id', auth()->user()->id)->first();
+        $firma = $usuario->firma;
+
+        //Formatea la fecha; Por adaptar para el informe sin comparacion
+        $fechaAnalisis = DB::table('ViewLoteAnalisis')->where('Id_lote', 0)->first();
+        if (!is_null($fechaAnalisis)) {
+            $fechaConFormato = date("d/m/Y", strtotime($fechaAnalisis->Fecha));
+        }
+
+        //BODY;Por añadir validaciones, mismas que se irán implementando cuando haya una tabla en la BD para los informes
+        $htmlInforme = view('exports.informes.sinComparacion.bodyInforme', /* compact('textoProcedimiento') */);
+
+        //HEADER-FOOTER******************************************************************************************************************                 
+        $htmlHeader = view('exports.informes.sinComparacion.headerInforme', /* compact('fechaConFormato') */);
+        $htmlFooter = view('exports.informes.sinComparacion.footerInforme', /* compact('usuario', 'firma') */);
+
+        $mpdf->setHeader("{PAGENO} / {nbpg} <br><br>" . $htmlHeader);
+        $mpdf->SetHTMLFooter($htmlFooter, 'O', 'E');
+        $mpdf->WriteHTML($htmlInforme);
+
+        $mpdf->CSSselectMedia = 'mpdf';
+        $mpdf->Output();
+    }
+
+    public function pdfComparacion(){
+        
     }
 }
--- routes/modules/informes.php
+++ routes/modules/informes.php
@@ -6,4 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'informes'], function () {
     Route::get('/', [InformesController::class, 'index']);
 
+    //Rutas temporales para la generación de plantillas de las bitácoras
+    Route::get('exportPdfSinComparacion', [InformesController::class, 'pdfSinComparacion']);
+    Route::get('exportPdfComparacion', [InformesController::class, 'pdfComparacion']);
 });
