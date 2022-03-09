a3233d912d305b7ec8443106b42f0538056f6e58
Christian Gonzalez <christian.gc099@gmail.com>
2022-03-08 18:01:55 -0600

    commit

--- resources/views/exports/informes/conComparacion/bodyInforme.blade.php
+++ resources/views/exports/informes/conComparacion/bodyInforme.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('/public/css/informes/conComparacion/conComparacion.css')}}">
-    <title>Informe Sin Comparación</title>
+    <title>Informe Con Comparación</title>
 </head>
 <body>
     <div id="contenedorTabla">
