665d237b5c6e4b73d33e48f6001b1391bc9abf94
LashGAMX <alber_199648@hotmail.com>
2022-03-30 17:57:27 -0600

    commit

--- routes/modules/supervicion.php
+++ routes/modules/supervicion.php
@@ -8,6 +8,7 @@ Route::group(['prefix' => 'supervicion'], function () {
         Route::get('cadenaCustodia', [CadenaCustodiaController::class, 'cadenaCustodia']);
         Route::get('detalleCadena/{id}', [CadenaCustodiaController::class, 'detalleCadena']);
         Route::post('getParametroCadena', [CadenaCustodiaController::class,'getParametroCadena']);
+        Route::post('getDetalleAnalisis', [CadenaCustodiaController::class,'getDetalleAnalisis']);
     });
 });
-    
+     
