5e815e8c5ab35fbbc379a09a4afb76b480b3dbe3
LashGAMX <alber_199648@hotmail.com>
2022-06-01 18:00:14 -0500

    commit

--- resources/views/vendor/voyager/master.blade.php
+++ resources/views/vendor/voyager/master.blade.php
@@ -34,7 +34,6 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="{{url('/public/assets/duallist/dist/bootstrap-duallistbox.min.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" class="">
-
     <!-- Few Dynamic Styles -->
     <style type="text/css">
         .voyager .side-menu .navbar-header {
