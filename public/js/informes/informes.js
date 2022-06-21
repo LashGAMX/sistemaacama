c1ad07166a42ba91a3d040123fba73a4d94f83a5
LashGAMX <alber_199648@hotmail.com>
2022-06-21 07:28:12 -0500

    commit

--- public/js/informes/informes.js
+++ public/js/informes/informes.js
@@ -30,9 +30,9 @@ $(document).ready(function () {
         console.log("Valor de tipoReporte: " + $("#tipoReporte").val());
 
         if($("#tipoReporte").val() == 1){
-            window.open(base_url+"/admin/informes/exportPdfConComparacion/"+idSol);
+            window.open(base_url+"/admin/informes/exportPdfConComparacion/"+idSol+"/"+$("#puntoMuestreo").val());
         }else if($("#tipoReporte").val() == 2){
-            window.open(base_url+"/admin/informes/exportPdfSinComparacion/"+idSol);
+            window.open(base_url+"/admin/informes/exportPdfSinComparacion/"+idSol+"/"+$("#puntoMuestreo").val());
         }        
     });
 
