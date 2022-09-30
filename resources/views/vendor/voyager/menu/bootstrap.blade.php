28449282816c2b4484b13202913e7f4602eae1f1
LashGAMX <alber_199648@hotmail.com>
2022-09-30 17:58:19 -0500

    commit

--- resources/views/vendor/voyager/menu/bootstrap.blade.php
+++ resources/views/vendor/voyager/menu/bootstrap.blade.php
@@ -1,88 +1,31 @@
-@if(!isset($innerLoop))
-<ul class="dd-list">
-@else
-<ul class="dropdown-menu">
-@endif
-
 @php
-
-    if (Voyager::translatable($items)) {
-        $items = $items->load('translations');
-    } 
-
+$menuP = DB::table('ViewMenuUsuarios')
+    ->where('parent_id', null)
+    ->where('Id_user', Auth::user()->id)
+    ->get();
+$menuH = DB::table('ViewMenuUsuarios')
+    ->where('Id_user', Auth::user()->id)
+    ->get();
 @endphp
-@foreach ($items as $item)
 
-    @php
+<ul>
 
-        $originalItem = $item;
-        if (Voyager::translatable($item)) {
-            $item = $item->translate($options->locale);
-        }
 
-        $listItemClass = null;
-        $linkAttributes =  null;
-        $styles = null;
-        $icon = null;
-        $caret = null;
+    @foreach ($menuP as $item)
 
-        // Background Color or Color
-        if (isset($options->color) && $options->color == true) {
-            $styles = 'color:'.$item->color;
-        }
-        if (isset($options->background) && $options->background == true) {
-            $styles = 'background-color:'.$item->color;
-        }
 
-        // With Children Attributes
-        if(!$originalItem->children->isEmpty()) {
-            $linkAttributes =  'class="dropdown-toggle" data-toggle="dropdown"';
-            $caret = '<span class="caret"></span>';
-
-            if(url($item->link()) == url()->current()){
-                $listItemClass = 'dropdown active';
-            }else{
-                $listItemClass = 'dropdown';
-            }
-        }
-
-        // Set Icon
-        if(isset($options->icon) && $options->icon == true){
-            $icon = '<i class="' . $item->icon_class . '"></i>';
-        }
- 
-    @endphp
-<div class="accordion" id="accordionExample" style="background: #353d47">
-
-    <div class="card"> 
-      <div class="card-header" id="headingTwo" style="background: #353d47">
-        <h2 class="mb-0">
-          <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            <a href="{{ url($item->link()) }}" target="{{ $item->target }}" style="{{ $styles }} color:#fff" {!! $linkAttributes ?? '' !!}>
-                <i class="{{$item->icon_class}}"></i>&nbsp;{{ $item->title }}</span>
+        <li class="">
+            <a href="{{ $item->route }}" target="{{ $item->target }}" >
+                t
+                <span>{{ $item->title }}</span>
             </a>
-          </button>
-        </h2>
-      </div>
-      @if(!$originalItem->children->isEmpty())
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-            <div class="card-body">
-                @foreach ($originalItem->children as $item2)
-                <div class="card">
-                    <div class="card-header" id="headingTwo">
-                      <h2 class="mb-0">
-                        <a href="{{ url($item2->link()) }}" style="font-size:50%;color:#fff;"><i class="{{$item2->icon_class}}"></i>&nbsp;<span>{{ $item2->title }}</span></a>
-                      </h2>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-        </div>
-      @endif
-    </div>
-
-</div>
-@endforeach
+            {{-- @if (!$originalItem->children->isEmpty())
+                @include('voyager::menu.default', [
+                    'items' => $originalItem->children,
+                    'options' => $options,
+                ])
+            @endif --}}
+        </li>
+    @endforeach
 
 </ul>
-
