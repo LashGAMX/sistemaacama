fff7682e64369674d6b0b57c7508362220db773d
LashGAMX <alber_199648@hotmail.com>
2022-10-10 18:02:55 -0500

    commit

--- resources/views/analisisQ/parametro.blade.php
+++ resources/views/analisisQ/parametro.blade.php
@@ -5,11 +5,30 @@
   @section('page_header')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
-            @livewire('analisis-q.parametros', ['idUser' => Auth::user()->id])
+      <div class="col-md-12">
+        <button></button>
+      </div>
+        <div class="col-md-12" id="divTabla">
+            <table class="table">
+              <tr>
+                <th>Id</th>
+                <th>Sucursal</th>
+                <th>Rama</th>
+                <th>Parámetro</th>
+                <th>Unidad</th>
+                <th>Método prueba</th>
+                <th>C. Metodo</th>
+                <th>Norma</th>
+                <th>Limite</th>
+                <th>Opc</th>
+              </tr>
+            </table>
         </div>
       </div>
 </div>
   @stop
-
+  @section('javascript')
+  <script src="{{asset('/public/js/analisisQ/parametro.js')}}?v=0.0.1"></script>
+@stop  
 @endsection
+ 
