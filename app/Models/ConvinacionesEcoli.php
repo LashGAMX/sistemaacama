809b354503b27b17809df925f9c27d82bb23a86b
IsaacYahirVillanuevaAlonso <isaacyannis@gmail.com>
2022-11-07 18:00:00 -0600

    commit

---
+++ app/Models/ConvinacionesEcoli.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ConvinacionesEcoli extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table = 'convinaciones_Ecoli';
+    protected $primaryKey = 'Id_convinacion';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'Id_detalle',
+        'Colonia',
+        'Indol',
+        'Rm',
+        'Vp',
+        'Citraro',
+        'BGN',
+        
+    ];
+}
