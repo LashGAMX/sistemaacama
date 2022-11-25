f82fd6aee1df66f72ea54fdf13aafca94c0b20fb
IsaacYahirVillanuevaAlonso <isaacyannis@gmail.com>
2022-11-24 18:04:45 -0600

    commit

---
+++ app/Models/Capsulas.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Capsulas extends Model
+{
+    use HasFactory,SoftDeletes;
+    protected $table = 'capsulas';  
+    protected $primaryKey = 'Id_capsula'; 
+    public $timestamps = true;
+
+    protected $fillable = [
+        'Num_serie',
+        'Peso',
+        'Min',
+        'Max',
+        'Estado'  
+    ];  
+}
