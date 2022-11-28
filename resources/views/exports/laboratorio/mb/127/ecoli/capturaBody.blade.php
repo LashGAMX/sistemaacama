468364664394ba6523568c369913ab36e59cf161
LashGAMX <alber_199648@hotmail.com>
2022-11-28 06:47:17 -0600

    commit

--- resources/views/exports/laboratorio/mb/127/ecoli/capturaBody.blade.php
+++ resources/views/exports/laboratorio/mb/127/ecoli/capturaBody.blade.php
@@ -18,37 +18,51 @@
     </div> 
     <br>
     <div id="contenedorTabla">
-        <br>
+        <br> 
 
         <table autosize="1" class="tabla" border="1">
             <thead>
                 <tr>
-                    <th colspan="4">Presuntiva</th>
-                    <th colspan="3">Confirmativa</th>
+                    <th colspan="7">E. Coli</th>
+                    <th colspan="2" rowspan="2">MOROFOLOGIA COLONIAL <br>EN AGAR EMB-L</th>
+                    <th colspan="10">PRUEBAS BIOQIMICAS</th>
+                    <th rowspan="2">Resultado <br> NMP</th>
                 </tr>
                 <tr>
-                    <th></th> 
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
+                    <th colspan="4" rowspan="2">Presuntiva</th>
+                    <th colspan="3" rowspan="2">Confirmativa</th>
+                    <th colspan="2" rowspan="2">INDOL</th>
+                    <th colspan="2" rowspan="2">RM</th>
+                    <th colspan="2" rowspan="2">VP</th>
+                    <th colspan="2" rowspan="2">CITRATO</th>
+                    <th colspan="2" rowspan="2">T.GRAM</th>
                 </tr>
+                
             </thead>
+            <tbody>
+                <tr>
+                    <td>Total tubos</td>
+                    <td>Positivo 24h</td>
+                    <td>Positivo 48h</td>
+                    <td>Resultado</td>
+                    <td>Positivo 24h</td>
+                    <td>Positivo 48h</td>
+                    <td>Resultado</td>
+                    <td>Tubo 1</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
         </table>
 
  
