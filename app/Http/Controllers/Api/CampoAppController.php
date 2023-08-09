ca04e9574c9d63765662c48ac3456e48c2af9a8a
IsaacYahirVillanuevaAlonso <isaacyannis@gmail.com>
2023-08-09 10:45:29 -0600

    commit

--- app/Http/Controllers/Api/CampoAppController.php
+++ app/Http/Controllers/Api/CampoAppController.php
@@ -271,9 +271,9 @@ class CampoAppController extends Controller
             $tempA3 = $jsonTempAmbiente[$i]["TempA3"];
             $promedioA = $jsonTempAmbiente[$i]["PromedioA"];
             
-            $tempAmbiente[$i]->Temperatura1 = floatval($tempA1);
-            $tempAmbiente[$i]->Temperatura2 = floatval($tempA2);
-            $tempAmbiente[$i]->Temperatura3 = floatval($tempA3);
+            $tempAmbiente[$i]->TemperaturaSin1 = floatval($tempA1);
+            $tempAmbiente[$i]->TemperaturaSin2 = floatval($tempA2);
+            $tempAmbiente[$i]->TemperaturaSin3 = floatval($tempA3);
             $tempAmbiente[$i]->Promedio = floatval($promedioA);
             $tempAmbiente[$i]->save();
         }
