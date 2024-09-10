a870eac4b3b4ec060b08fafd584dc717ac474842
LashGAMX <alber_199648@hotmail.com>
2024-09-10 13:48:08 -0600

    commit  norma

--- resources/views/vendor/voyager/master.blade.php
+++ resources/views/vendor/voyager/master.blade.php
@@ -42,8 +42,8 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     
     <link rel="stylesheet" href="{{asset('/public/assets/duallist/dist/bootstrap-duallistbox.min.css')}}">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" class=""> -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" class="">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> -->
 
     
     <!-- Few Dynamic Styles -->
@@ -156,7 +156,7 @@ $(document).ready( function () {
 
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-<!-- <script src="{{asset('public/assets/duallist/dist/jquery.bootstrap-duallistbox.min.js')}}?v=0.0.2"></script>  -->
+<script src="{{asset('public/assets/duallist/dist/jquery.bootstrap-duallistbox.min.js')}}?v=0.0.2"></script> 
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script> -->
 
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
@@ -164,7 +164,7 @@ $(document).ready( function () {
 
 
 <script src="{{asset('public/js/notificaciones/notificacion.js')}}"></script> 
-<!-- <script src="{{asset('public/js/chat/mensaje.js')}}?v=0.0.1"></script>  -->
+<script src="{{asset('public/js/chat/mensaje.js')}}?v=0.0.1"></script> 
 
 
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
@@ -207,7 +207,7 @@ $(document).ready( function () {
 @if(!empty(config('voyager.additional_js')))<!-- Additional Javascript -->
     @foreach(config('voyager.additional_js') as $js)<script type="text/javascript" src="{{ asset($js) }}"></script>@endforeach
 @endif
-
+ 
 
 
 </body>
